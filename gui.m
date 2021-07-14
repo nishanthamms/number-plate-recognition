a3d3c03d6a4df30690ce67688fdabd9f72695662
Nishantha <nishanthamms@gmail.com>
2021-07-14 19:26:12 +0530

    initial commit

---
+++ gui.m
@@ -0,0 +1,188 @@
+function varargout = gui(varargin)
+
+% GUI MATLAB code for gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui
+
+% Last Modified by GUIDE v2.5 14-Jul-2021 18:41:30
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui is made visible.
+function gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui (see VARARGIN)
+
+% Choose default command line output for gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+load imgfildata;
+set(handles.axes1,'XTick',[], 'YTick', [])
+% UIWAIT makes gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[file,path]=uigetfile({'*.jpg;*.bmp;*.png;*.tif'},'Choose an image');
+s=[path,file];
+picture=imread(s);
+[~,cc]=size(picture);
+picture=imresize(picture,[300 500]);
+axes(handles.axes1);
+imshow(picture);
+%imgray = rgb2gray(im1);
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+load imgfildata;
+picture=getimage(handles.axes1);
+if size(picture,3)==3
+  picture=rgb2gray(picture);
+end
+% se=strel('rectangle',[5,5]);
+% a=imerode(picture,se);
+% figure,imshow(a);
+% b=imdilate(a,se);
+threshold = graythresh(picture);
+picture =~im2bw(picture,threshold);
+picture = bwareaopen(picture,30);
+imshow(picture)
+%if cc>2000
+ %   picture1=bwareaopen(picture,3500);
+%else
+%picture1=bwareaopen(picture,3000);
+%end
+picture1=bwareaopen(picture,3500);
+axes(handles.axes1);
+imshow(picture1)
+%figure,imshow(picture1)
+picture2=picture-picture1;
+%figure,imshow(picture2)
+axes(handles.axes1);
+imshow(picture2)
+picture2=bwareaopen(picture2,200);
+%figure,imshow(picture2)
+axes(handles.axes1);
+imshow(picture2)
+
+%MARKE VALUES IN GREEN COLOR
+[L,Ne]=bwlabel(picture2);
+propied=regionprops(L,'BoundingBox');
+hold on
+pause(1)
+for n=1:size(propied,1)
+  rectangle('Position',propied(n).BoundingBox,'EdgeColor','g','LineWidth',2)
+end
+hold off
+
+figure
+final_output=[];
+t=[];
+for n=1:Ne
+  [r,c] = find(L==n);
+  n1=picture(min(r):max(r),min(c):max(c));
+  n1=imresize(n1,[42,24]);
+  imshow(n1)
+  pause(0.2)
+  x=[ ];
+
+totalLetters=size(imgfile,2);
+
+ for k=1:totalLetters
+    
+    y=corr2(imgfile{1,k},n1);
+    x=[x y];
+    
+ end
+ t=[t max(x)];
+ if max(x)>.40
+ z=find(x==max(x));
+ out=cell2mat(imgfile(2,z));
+
+final_output=[final_output out];
+end
+end
+
+set(handles.text3,'String',final_output);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+ cla(handles.axes1,'reset');
+ set(handles.text3,'string',' ');
+ set(handles.axes1,'XTick',[], 'YTick', [])
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+closereq(); 
